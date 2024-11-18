fce872fecabf72ffefa3569191e1f29621fdefd7
Juliana <julianajesus06@hotmail.com>
2024-11-18 15:19:29 -0300

    topico4_CriandoAplicacaoMVC

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -1,8 +1,11 @@
 <x-layout title="Cadastro de  Série">
 
-<form action="" method="post">
-    <label for="nome">Nome da série</label>
-    <input type="text" name="nome" id="nome">
-</form>
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome da série</label>
+            <input type="text" name="nome" id="nome" class="form-control">
+        </div>
+        <button type="button" class="btn btn-info">Adicionar</button>
+    </form>
 
 </x-layout>
--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout title="Controle de Séries">
-    <a href="/series/criar">Adicionar</a>
+    <a href="/series/criar" class="btn btn-success mb-5">Adicionar</a>
 
-    <ul>
+    <ul class="list-group">
         @foreach ($series as $serie)
-        <li>{{$serie}}</li>
+        <li class="list-group-item">{{$serie}}</li>
         @endforeach
     </ul>
 </x-layout>
