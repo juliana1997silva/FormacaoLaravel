26c17406556ea4944644bc3efe824ec3581f18c5
Juliana <julianajesus06@hotmail.com>
2024-11-19 16:01:08 -0300

    03_ValidandoDados

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Cadastro de  Série">
-<x-form :action="route('series.store')" nomeButton="Adicionar"/>
+    <x-form :action="route('series.store')" nomeButton="Adicionar" :nome="old('nome')" :update="false"/>
 </x-layout>
--- controle-series/resources/views/series/edit.blade.php
+++ controle-series/resources/views/series/edit.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Editar Série - {{$serie->nome}}">
 
-    <x-form :action="route('series.update', $serie->id)" :nome="$serie->nome" nomeButton="Editar" />
+    <x-form :action="route('series.update', $serie->id)" :nome="$serie->nome" nomeButton="Editar" :update="true" />
 
 </x-layout>
