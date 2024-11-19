7a2e2dda07f769bfefbbf5860d40258a8828ded8
Juliana <julianajesus06@hotmail.com>
2024-11-19 09:33:17 -0300

    topico5_CriandoAplicacaoMVC

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -1,11 +1,12 @@
 <x-layout title="Cadastro de  Série">
 
-    <form action="" method="post">
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome da série</label>
             <input type="text" name="nome" id="nome" class="form-control">
         </div>
-        <button type="button" class="btn btn-info">Adicionar</button>
+        <button type="submit" class="btn btn-info">Adicionar</button>
     </form>
 
 </x-layout>
