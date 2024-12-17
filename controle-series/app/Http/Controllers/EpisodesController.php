982353f8a631be9c5321b07333afed46d4e7f250
Juliana <julianajesus06@hotmail.com>
2024-12-17 18:02:01 -0300

    autenticação

--- controle-series/app/Http/Controllers/EpisodesController.php
+++ controle-series/app/Http/Controllers/EpisodesController.php
@@ -6,7 +6,7 @@ use App\Models\Episode;
 use App\Models\Season;
 use Illuminate\Http\Request;
 
-class EpisodesController
+class EpisodesController extends Controller
 {
     public function index(Season $season)
     {
--- controle-series/resources/views/components/layout.blade.php
+++ controle-series/resources/views/components/layout.blade.php
@@ -9,6 +9,14 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="{{route('series.index')}}">Home</a>
+   <a href="{{route('logout')}}">Sair</a>
+    
+    </div>
+  </div>
+</nav>
 
     <div class="container">
         <h1>{{ $title }}</h1>
